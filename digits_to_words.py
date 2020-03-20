1a5d2a4ed6a5cd94671dadef27297843019f7682
cValda <p.valosek@gmail.com>
2020-03-20 23:00:25 +0100

    Added digits_to_words

---
+++ digits_to_words.py
@@ -0,0 +1,18 @@
+#this code should replace digits in a string with respective words - needs work
+
+phrase = str(input())
+
+print(phrase)
+
+zero = phrase.replace('0', 'zero')
+one = zero.replace('1', 'one')
+two = one.replace('2', 'two')
+three = two.replace('3', 'three')
+four = three.replace('4', 'four')
+five = four.replace('5', 'five')
+six = five.replace('6', 'six')
+seven = six.replace('7', 'seven')
+eight = seven.replace('8', 'eight')
+nine = eight.replace('9', 'nine')
+
+print(nine)

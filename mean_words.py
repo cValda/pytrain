8257cd170e210a7f299fac2f54f9a40a7dd923dc
cValda <p.valosek@gmail.com>
2020-03-22 15:22:35 +0100

    Fixed a math error in mean_words

--- mean_words.py
+++ mean_words.py
@@ -8,10 +8,10 @@ def mean_words(t):
     depunct = [i for i in chars if i.isalpha() or i == ' ']
     joined = ''.join(depunct)
     split = joined.split(' ')
+    a = 0
+    b = 0
     for i in split:
-        a = 0
         a += len(i)
-        b = 0
         b += 1
         mean = math.ceil(a / b)
     return mean
